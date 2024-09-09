5aa1cc7a3b7f4de791ab15da9122623ac8b1a7c5
BrianUncc <brianvready@gmail.com>
2024-09-08 23:06:42 -0400

    Timestamp 4
    1:39:00 - CRUD

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path
-from .views import home, room
+from base import views
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('room/<str:pk>/', room, name='room'),
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('room/<str:pk>/', views.room, name='room'),
 ]
