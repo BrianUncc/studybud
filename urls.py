0f03b102268b961db0beb88a55b4b8231f314af0
BrianUncc <brianvready@gmail.com>
2024-09-26 21:51:32 -0400

    Part 3 Timestamp 1
    5:11:50 - Edit User Account Page

--- urls.py
+++ urls.py
@@ -17,7 +17,6 @@ urlpatterns = [
     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
     path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
 
+    path('update-user/', views.updateUser, name="update-user"),
+
 ]
-
-path('profile/<str:pk>/', views.userProfile, name="user-profile"),
-
